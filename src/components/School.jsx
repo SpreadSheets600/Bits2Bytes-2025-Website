4152bc8e2996a6066e07bc5558d0e7b9c942f84d
HRISHIKESH DAS <dashrishikesh900@gmail.com>
2025-07-29 02:03:39 +0530

    Feat: School Updated

--- src/components/School.jsx
+++ src/components/School.jsx
@@ -3,7 +3,7 @@ export default function School() {
 
     return (
         <>
-        <h1 className="text-5xl text-white font-clash font-semibold">School Section</h1>
+        <h1 className="text-5xl text-white font-clash font-semibold">School Section Comming Soon</h1>
         </>
     );
 }
